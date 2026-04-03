ab4374572232700ace2575745f1c8d3f2c2b7234
FelipeSanches <felipepvpsanches3@gmail.com>
2026-04-03 19:24:57 -0300

    feat: inicio de estilização e criação da página home
    feat: implementação de imagem e criação de css modulo para app

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,16 +2,37 @@
 import styles from './Home.module.css'
 
 export default function Home() {
+  const irParaLoja = () => {
+    window.location.href = '/loja'
+  }
+
   return (
     <main className={styles.main}>
-      <div className={styles.conteudo}>
-        <h1 className={styles.titulo}>Quem somos?</h1>
-        <p className={styles.texto}>
-          A Terabyte é a atlética oficial do curso de Ciência da Computação do IF Goiano - Campus
-          Morrinhos. Fundada em 2018, a Atlética tem como objetivo promover a integração entre os
-          estudantes do curso, além de incentivar a prática esportiva e a participação em eventos
-          acadêmicos e sociais.
-        </p>
+      <div className={styles.logo}>
+        <img className={styles.image} src="./images/logo_bombado.png" alt="Logo da Terabyte" />
+      </div>
+      <div className={styles.colunaDireita}>
+        <div className={styles.conteudo}>
+          <h1 className={styles.titulo}>Quem somos?</h1>
+          <p className={styles.texto}>
+            A Terabyte é a atlética oficial do curso de Ciência da Computação do IF Goiano - Campus
+            Morrinhos. Fundada em 2018, a Atlética tem como objetivo promover a integração entre os
+            estudantes do curso, além de incentivar a prática esportiva e a participação em eventos
+            acadêmicos e sociais.
+          </p>
+        </div>
+        <div className={styles.produtos}>
+          <p className={styles.titulo}>Produtos</p>
+          <ul className={styles.lista}>
+            <li>Camisa</li>
+            <li>Caneca</li>
+            <li>Adesivo</li>
+          </ul>
+
+          <button className={styles.botao} onClick={irParaLoja}>
+            Acessar Loja!
+          </button>
+        </div>
       </div>
     </main>
   )
